85be089ec93b550f61da84b668813e5e7d24f946
Marc Geweiler <Marcgeweiler@web.de>
2025-01-25 18:44:49 +0100

    clean copy button animation

--- static/script.js
+++ static/script.js
@@ -1,15 +1,18 @@
 
-function copyToClipboard(elementId)
- {
-     const textToCopy = document.getElementById(elementId).innerText;
-
-    navigator.clipboard.writeText(textToCopy).then(() => 
-    { 
-       alert("Copied to clipboard!");                          //needs improvement 
-    
+function copyToClipboard(elementId, button) {
+    const textToCopy = document.getElementById(elementId).innerText;
+  
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      
+      button.classList.add("copied");
+  
+     
+      setTimeout(() => {
+        button.classList.remove("copied");
+      }, 1000);
     }).catch((err) => {
-        console.error("Failed to copy: ", err);
+      console.error("Failed to copy: ", err);
     });
-}
+  }
 
 
